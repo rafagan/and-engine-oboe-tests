4bc59dd79f36841ec8f33f89c8e022f372590fee
Rafagan Abreu <rafagan@guizion.com>
2018-11-22 18:22:05 -0200

    Novas tentativas de tocar wav e ogg

--- TileMusicGame/app/src/main/cpp/AudioPlayer.cpp
+++ TileMusicGame/app/src/main/cpp/AudioPlayer.cpp
@@ -13,7 +13,7 @@ constexpr int kChannelCount{2};
 AudioPlayer::AudioPlayer(AAssetManager* assetManager)
         : assetManager(assetManager)
 {
-    backingTrack = SoundRecording::loadFromAssets(assetManager, "music.raw");
+    backingTrack = SoundRecording::loadFromAssets(assetManager, "whistle.raw");
     clap = SoundRecording::loadFromAssets(assetManager, "clap.raw");
 }
 
@@ -24,20 +24,20 @@ void AudioPlayer::playSound() {
     // Create a builder
     AudioStreamBuilder builder;
     builder.setFormat(AudioFormat::I16);
-    builder.setChannelCount(2);
+    builder.setChannelCount(kChannelCount);
     builder.setSampleRate(kSampleRateHz);
     builder.setCallback(this);
     builder.setPerformanceMode(PerformanceMode::LowLatency);
     builder.setSharingMode(SharingMode::Exclusive);
 
     Result result = builder.openStream(&audioStream);
-    if (result != Result::OK){
+    if (result != Result::OK) {
         LOGE("Failed to open stream. Error: %s", convertToText(result));
     }
 
     // Reduce stream latency by setting the buffer size to a multiple of the burst size
     auto setBufferSizeResult = audioStream->setBufferSizeInFrames(audioStream->getFramesPerBurst() * kBufferSizeInBursts);
-    if (setBufferSizeResult != Result::OK){
+    if (setBufferSizeResult != Result::OK) {
         LOGW("Failed to set buffer size. Error: %s", convertToText(setBufferSizeResult.error()));
     }
 
@@ -49,7 +49,7 @@ void AudioPlayer::playSound() {
 
 DataCallbackResult AudioPlayer::onAudioReady(AudioStream *oboeStream, void *audioData, int32_t numFrames) {
     for (int i = 0; i < numFrames; ++i) {
-        backingTrack->renderAudio(static_cast<int16_t*>(audioData)+(kChannelCount*i), 1);
+        backingTrack->renderAudio(static_cast<int16_t*>(audioData) + (kChannelCount * i ), 1);
     }
 
     return DataCallbackResult::Continue;
